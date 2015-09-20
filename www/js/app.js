68a7a9f23421087bbbe983d4937ecca719d8b6e2
Marc J. Schmidt <marc@marcjschmidt.de>
2015-09-21 00:02:39 +0200

    works with sounds, yeah

--- www/js/app.js
+++ www/js/app.js
@@ -18,12 +18,199 @@ var lockMe = angular.module('lock-me', ['ionic'])
         });
     });
 
-lockMe.controller('LockerController', function($scope){
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+lockMe.controller('SinglePlayerController', function ($scope, $timeout) {
+    $scope.level = 1;
+});
+
+lockMe.directive("onTouchstart", function () {
+    return {
+        controller: function ($scope, $element, $attrs) {
+            $element.bind('touchstart', onTouchStart);
+
+            function onTouchStart(event) {
+                var method = $element.attr('on-touchstart');
+                $scope.$event = event;
+                $scope.$apply(method);
+            }
+        }
+    }
+});
+
+lockMe.controller('LockerController', function ($scope, $timeout) {
     $scope.lastLockInnerStep = 0;
 
-    $scope.handleDrag = function($event) {
-        $scope.lockInnerStep = Math.floor($scope.lastLockInnerStep + ($event.gesture.deltaY / 5));
-        console.log($event.gesture.deltaY, $scope.lockInnerStep);
-        $scope.lockInnerStepClass = 'lock-inner-rotate-step-' + $scope.lockInnerStep;
+    $scope.step = 2;
+
+    var steps = 24;
+
+    var knob = document.getElementById('lock-knob-items');
+    $scope.handleDrag = function ($event) {
+        $scope.lockInnerStep = Math.floor($scope.lastLockInnerStep + $event.gesture.deltaY);
+        knob.style.transform = 'rotate(' + $scope.lockInnerStep + 'deg)';
+        knob.style.webkitTransform = 'rotate(' + $scope.lockInnerStep + 'deg)';
+        //console.log($scope.lockInnerStep);
+        $scope.step = $scope.lockInnerStep;
+        //$scope.lockInnerStepClass = 'lock-inner-rotate-step-' + $scope.lockInnerStep;
+    };
+
+    function getAngle(el) {
+        var st = window.getComputedStyle(el, null);
+        var tr = st.getPropertyValue("-webkit-transform") ||
+            st.getPropertyValue("-moz-transform") ||
+            st.getPropertyValue("-ms-transform") ||
+            st.getPropertyValue("-o-transform") ||
+            st.getPropertyValue("transform") ||
+            null;
+
+        if (!tr || tr === 'none') {
+            return false;
+        }
+
+        var values = tr.split('(')[1];
+        values = values.split(')')[0];
+        values = values.split(',');
+        var a = values[0];
+        var b = values[1];
+        var c = values[2];
+        var d = values[3];
+
+        var scale = Math.sqrt(a * a + b * b);
+
+        return Math.round(Math.atan2(b, a) * (180 / Math.PI));
     }
+
+    $scope.runs = false;
+    $scope.stepsNeeded = $scope.$parent.level;
+
+    $scope.active = true;
+    var tolerance = 7;
+    var lastFailureTimeout;
+
+    $scope.handleTouch = function ($event) {
+        if ($scope.failed || !$scope.active) {
+            return;
+        }
+
+        console.log('touch');
+        $timeout.cancel(lastFailureTimeout);
+
+        if (!$scope.runs) {
+            console.log('handle start');
+            $scope.nextStep(true);
+            $scope.runs = true;
+        } else {
+            var angle = getAngle(knob);
+
+            console.log('computed angle', angle);
+
+            if (angle >= -tolerance && angle <= tolerance) {
+                $scope.stepsNeeded--;
+
+                ion.sound.play("lock" + getRandomIntInclusive(1,3));
+
+                if (0 === $scope.stepsNeeded) {
+                    $scope.runs = false;
+                    knob.style.transform = 'rotate(' + angle + 'deg)';
+                    knob.style.webkitTransform = 'rotate(' + angle + 'deg)';
+                    $scope.succeeded = true;
+                    $scope.$parent.level++;
+                    return;
+                }
+
+                $scope.nextStep();
+            } else {
+                knob.style.transform = 'rotate(' + angle + 'deg)';
+                knob.style.webkitTransform = 'rotate(' + angle + 'deg)';
+                $scope.failure();
+            }
+        }
+    };
+
+    ion.sound({
+        sounds: [
+            {
+                name: "lock1"
+            },
+            {
+                name: "lock2"
+                //volume: 0.2
+            },
+            {
+                name: "lock3"
+                //volume: 0.3,
+                //preload: false
+            }
+        ],
+        volume: 0.5,
+        path: "sounds/",
+        preload: true
+    });
+
+    $scope.lastStep = 0;
+    $scope.restart = function () {
+        console.log('restart');
+        $scope.active = true;
+        $scope.stepsNeeded = $scope.$parent.level;
+        $scope.failed = false;
+        $scope.runs = true;
+        $scope.succeeded = false;
+        $scope.nextStep();
+    };
+
+    $scope.failure = function() {
+        $scope.runs = false;
+        $scope.failed = true;
+        $scope.active = false;
+
+        $timeout(function(){
+            $scope.active = true;
+        }, 100);
+    };
+
+
+    $scope.nextStep = function (firstRun) {
+        if (firstRun) {
+            $scope.targetStep = getRandomIntInclusive(9, steps - 9);
+        } else {
+            $scope.targetStep = getRandomIntInclusive(3, steps - 3);
+        }
+        $scope.targetPosition = $scope.targetStep * (360 / steps);
+
+        console.log('next step', $scope.step, $scope.targetPosition);
+
+        knob.style.webkitTransitionDuration = '0s';
+        knob.style.transitionDuration = '0s';
+
+        knob.style.transform = 'rotate(' + $scope.targetPosition + 'deg)';
+        knob.style.webkitTransform = 'rotate(' + $scope.targetPosition + 'deg)';
+
+        console.log('go random', knob.style.transform);
+        $timeout(function () {
+            var timePerStep = 0.15;
+
+            var targetDeg = -tolerance;
+
+            var timeTotal = $scope.targetStep * timePerStep;
+
+            if ($scope.targetStep > steps / 2) {
+                targetDeg = 360 + tolerance;
+                timeTotal = (steps - $scope.targetStep) * timePerStep;
+            }
+
+            lastFailureTimeout = $timeout(function () {
+                $scope.failure();
+            }, timeTotal * 1000);
+
+            knob.style.webkitTransitionDuration = timeTotal + 's';
+            knob.style.transitionDuration = timeTotal + 's';
+
+            knob.style.transform = 'rotate(' + targetDeg + 'deg)';
+            knob.style.webkitTransform = 'rotate(' + targetDeg + 'deg)';
+            console.log('go back', knob.style.transform, timeTotal);
+        }, 50);
+    };
 });
